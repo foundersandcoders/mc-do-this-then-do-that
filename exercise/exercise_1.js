065cdcfb51715693881da45c388b1272a2400019
Matteo Giaccone <matteo@turnintocoders.it>
2018-02-07 23:16:58 +0100

    Rephrasing and typos

--- exercise/exercise_1.js
+++ exercise/exercise_1.js
@@ -1,11 +1,11 @@
 var divide = function(x, y) {
-  // todo divide the numbers and handle the case where y is zero (see the tests)
+  // TODO: divide the numbers and handle the case where y is zero (see the tests)
 }
 
 var divideByThenAdd = function(x, y, z) {
   var division = divide(x, y)
 
-  // todo handle the case where divide returns an error
+  // TODO: handle the case where divide returns an error
 
   return division + z
 }
